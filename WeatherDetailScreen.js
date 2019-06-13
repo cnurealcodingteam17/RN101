b98791da1680604dd47dc3331e214061c477f071
kwakjuheon <juhun2005@naver.com>
2019-06-13 20:01:09 +0900

    change the stylesheet

--- WeatherDetailScreen.js
+++ WeatherDetailScreen.js
@@ -25,11 +25,11 @@ export default class WeatherDetailScreen extends React.Component {
     const { navigation } = this.props;
     const city = navigation.getParam('city', null);
     // const city = 'Daejeon';
-    // console.log(city);
+
 
     // ( 아이피 주소로 링크를 만들어야 함니당 ) -> 링크에 아이피주소 따올 수 있는 방법을 나중에 찾아보죠
-    const cityuri = `http://192.168.1.163:8080/weather-crawler/current-weathers/by-city-name/${city}`
-    // console.log(cityuri)
+    const cityuri = `http://192.168.1.150:8080/weather-crawler/current-weathers/by-city-name/${city}`
+
     fetch(cityuri)
       .then(response => response.json())
       .then(info => {
@@ -51,33 +51,22 @@ export default class WeatherDetailScreen extends React.Component {
 
     let weatherMain = this.state.weather[0].main;
     let celsius = this.state.main.temp - 273.15;
-    let yuna = this.state.wind.speed;
+    let wind_speed = this.state.wind.speed;
     let iconId = this.state.weather[0].icon;
 
-  // const iconW = `http://openweathermap.org/img/w/${iconId}.png`
-  // // console.log(cityuri)
-  // fetch(iconW)
-  //   .then(response => response.json())
-  //   .then(json => {
-  //
-  //   });
-//console.log(iconId);
-      // <Icon  IconId = {iconId} />
-    return (
 
+    return (
       <View style={styles.container} >
 
             <View style={styles.upper}>
             <Image
-            style ={{width: 150, height: 150}}
-            source = {{uri: `http://openweathermap.org/img/w/${iconId}.png`}}
+              style = {styles.image}
+              source = {{uri: `http://openweathermap.org/img/w/${iconId}.png`}}
             />
-            <Text style={styles.font1}> { weatherMain }</Text>
-            <Text>온도: {celsius.toFixed(1)} ℃</Text>
-            <Text>바람: {yuna.toFixed(1)} m/s</Text>
+            <Text style={styles.weather}> { weatherMain } </Text>
+            <Text style={styles.temp}> 온도: {celsius.toFixed(1)} ℃ </Text>
+            <Text style={styles.wind}>바람: {wind_speed.toFixed(1)} m/s </Text>
             </View>
-
-
 
       </View >
     );
@@ -90,41 +79,49 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight,
-     alignItems:'center',
+    marginTop: -10,
+    alignItems:'center',
   },
   upper: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "transparent"
-},
-temp: {
-    fontSize: 48,
+  },
+  image:{
+    width: 150, height: 150,
+    marginTop: 0
+  },
+  temp: {
+    fontSize: 20,
     backgroundColor: "transparent",
     color: "black",
     marginTop: 10
-},
-lower: {
+  },
+  lower: {
     flex: 1,
     alignItems: "flex-start",
     justifyContent: "flex-end",
     paddingLeft: 25
-},
-title: {
+  },
+  title: {
     fontSize: 38,
     backgroundColor: "transparent",
     color: "black",
     marginBottom: 10,
     fontWeight: "300"
-},
-subtitle: {
+  },
+  subtitle: {
     fontSize: 24,
     backgroundColor: "transparent",
     color: "black",
     marginBottom: 24
-},
-  font1: {
-        fontSize: 35
   },
+  weather: {
+    fontSize: 35
+  },
+  wind:{
+    fontSize: 20,
+    marginTop: 10
+  }
 });
